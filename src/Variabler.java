a5c1f1e38dd34865b56359c5b664a86ffb8ffaf7
Lisa-kode <lisa6262@edu.easj.dk>
2019-09-09 10:01:09 +0200

    Noter fra undervisnignen

---
+++ src/Variabler.java
@@ -0,0 +1,26 @@
+public class Variabler {
+
+    public static void main(String[] args) {
+
+        //primitive datatyper
+        byte a = 123;
+        int b = 1024;
+        short c = 1001;
+
+        //decimaltal
+        double d = 2.7;
+        float e = 9.4f;
+
+        //Boolean
+        boolean f = true;
+        boolean g = false;
+
+        // String
+        String h = "Hest";
+
+        //Char
+        char i = 'b';
+
+        System.out.println(a);
+    }
+}
